7a207bb7a9cd62afc894df9b3132cf61fe7f3282
wolfsl <tharindupriyashan18@gmail.com>
2022-03-06 13:40:25 +0530

    2022/03/06 responsive upgrade

--- js/animation.js
+++ js/animation.js
@@ -1,59 +1,126 @@
 
-     const next=document.querySelector(".popular-articles .nav-btns #next");
-     const prev=document.querySelector(".popular-articles .nav-btns #prev");
+      const next=document.querySelector(".popular-articles .nav-btns #next");
+      const prev=document.querySelector(".popular-articles .nav-btns #prev");
+      const slider=document.querySelector("#slide-bar");
+     const mediaQuery1 = window.matchMedia('(min-device-width: 374px) and (max-device-width: 668px) and (-webkit-min-device-pixel-ratio: 2)')
+     const mediaQuery2 = window.matchMedia('(min-device-width: 669px)')
+     // Check if the media query is true
+     if (mediaQuery1.matches) {
 
-     const slider=document.querySelector("#slide-bar");
-
-    //  const slider_pos=slider.getBoundingClientRect();
-    //  var style = slider.currentStyle || window.getComputedStyle(slider);
-    //  console.log("left: "+slider_pos.left);
-    //  console.log("right: "+slider_pos.right);
-    //  console.log("left: "+style.marginLeft);
-    //  console.log("right: "+style.marginRight);
-     let pos=0;
-
-    next.addEventListener("click",()=>{
-
-    pos-=400;
-      if(pos==-800){
-
-        document.getElementById("next").disabled = true;
-        
-      }
+       //  console.log("right: "+style.marginRight);
+       let pos=0;
  
-
-    slider.style.transform=`translateX(${pos}px)`;
-    document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
-
-
-
-
-});
-      if(pos==0){
-
-          document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
-
-      }
-
-      
-      
-    prev.addEventListener("click",()=>{
-      pos+=400;
+       next.addEventListener("click",()=>{
+   
+       pos-=275;
+         if(pos==-1100){
+   
+           document.getElementById("next").disabled = true;
+           
+         }
+    
+   
+       slider.style.transform=`translateX(${pos}px)`;
+       document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
+   
+   
+   
+   
+   });
+         if(pos==0){
+   
+             document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
+   
+         }
+   
+         
+         
+       prev.addEventListener("click",()=>{
+         pos+=275;
+        
+   
+           if(pos==0){
+   
+             document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
+   
+             }
+             else{
+   
+               document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
+             }
+         slider.style.transform=`translateX(${pos}px)`;
+         console.log(pos);
+   
      
-
-        if(pos==0){
-
-          document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
-
-          }
-          else{
-
-            document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
-          }
-      slider.style.transform=`translateX(${pos}px)`;
-      console.log(pos);
+         document.getElementById("next").disabled = false;
+   
+       });
+   
+       // Then trigger an alert
+       
+     }
+    
+    else if(mediaQuery2.matches){
 
   
-      document.getElementById("next").disabled = false;
+ 
+     //  const slider_pos=slider.getBoundingClientRect();
+     //  var style = slider.currentStyle || window.getComputedStyle(slider);
+     //  console.log("left: "+slider_pos.left);
+     //  console.log("right: "+slider_pos.right);
+     //  console.log("left: "+style.marginLeft);
+     //  console.log("right: "+style.marginRight);
+      let pos=0;
+ 
+     next.addEventListener("click",()=>{
+ 
+     pos-=400;
+       if(pos==-800){
+ 
+         document.getElementById("next").disabled = true;
+         
+       }
+  
+ 
+     slider.style.transform=`translateX(${pos}px)`;
+     document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
+ 
+ 
+ 
+ 
+ });
+       if(pos==0){
+ 
+           document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
+ 
+       }
+ 
+       
+       
+     prev.addEventListener("click",()=>{
+       pos+=400;
+      
+ 
+         if(pos==0){
+ 
+           document.querySelector(".popular-articles .nav-btns #prev").disabled = true;
+ 
+           }
+           else{
+ 
+             document.querySelector(".popular-articles .nav-btns #prev").disabled = false;
+           }
+       slider.style.transform=`translateX(${pos}px)`;
+       console.log(pos);
+ 
+   
+       document.getElementById("next").disabled = false;
+ 
+     });
+ 
 
-    });
+    }
+    
+    
+    
+ 
