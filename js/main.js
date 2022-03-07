e03ce23c0bb44b258af0c6ae4e242a01f2d06a4a
wolfsl <tharindupriyashan18@gmail.com>
2022-03-07 15:01:32 +0530

    upgrade 2022/03/07 04.30pm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-// $(document).ready(function(){
-//     $("#prev").click(function(){
-//       $("#slide-bar").animate({left: '250px'});
-//     });
-//   });
+const cb = document.querySelector('footer .main-content .row-1 .col-1 #ul-0');
+
+  alert(cb.checked)  ;
+
+        //    {     document.querySelector("footer .main-content #ul-0").display=block;
+
+        //         alert("display blockQ!");
+        //         console.log("This is working");
+        // }
