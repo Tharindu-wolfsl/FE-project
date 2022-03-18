144008f5658f4ad074ca593a9a06b40d4625ae04
wolfsl <tharindupriyashan18@gmail.com>
2022-03-18 18:28:28 +0530

    second page mobile view finished 2022/03/18

--- js/pop.js
+++ js/pop.js
@@ -1,3 +1,20 @@
+
 document.querySelector('.drop-list .selection #radio1').checked=true;
 
+document.querySelector('.section-1 .list .item #drop-1').onclick=function(){
+
+    document.getElementById("list").style.visibility="hidden";
+    document.getElementById("drop-list").style.visibility="visible";
     
+};
+document.querySelector('.section-1 .list .item .drop-list #close').onclick=function(){
+
+    document.getElementById("list").style.visibility="visible";
+    document.getElementById("drop-list").style.visibility="hidden";
+    
+};
+
+
+
+
+
