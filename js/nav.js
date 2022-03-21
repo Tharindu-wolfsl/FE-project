5e696c5ff0b30a3cc733cc31389b280c0fb41957
wolfsl <tharindupriyashan18@gmail.com>
2022-03-21 14:56:48 +0800

    headers updated

--- js/nav.js
+++ js/nav.js
@@ -11,12 +11,14 @@ if(cn.checked==true){
 
 
     document.getElementById("mob-nav").style.display="block";
+    document.getElementById("mob-nav").style.width="80%"
     document.getElementById("mob-nav").style.height="fit-content";
     document.getElementById("mob-nav").style.opacity="1";
 
 }else{
 
     document.getElementById("mob-nav").style.display="none";
+    document.getElementById("mob-nav").style.width="0%"
 
 }
 
